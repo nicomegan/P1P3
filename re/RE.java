c341f345d35bf46d1e244fedfef778cd9e6b836f
Anne Brinegar <AnneBrinegar@u.boisestate.edu>
2018-04-25 11:34:24 -0600

    added some parser methods and got rid of red

--- re/RE.java
+++ re/RE.java
@@ -1,39 +1,110 @@
 package re;
 
-import javax.sound.midi.Sequence;
-
 import fa.nfa.NFA;
 
 public class RE implements REInterface {
 
-	private static RE blank;
+	private static Blank blank;
 	private String regEx;
 
 	public RE(String regEx) {
 		this.regEx = regEx;
 	}
 
+	//will do parse
 	@Override
 	public NFA getNFA() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/* TODO Decent parser!!! */
-	// Not sure if needed
+	/* TODO Decent parser!!!
+	REgular expression term types 
+	Specifies if loop,mult options,etc??? */
 
-	
-	//not sure what this is
-	public RE parse() {
-		return null;
+	/**
+	 * gets the next regular expression
+	 * 
+	 * @return - term/regular expression
+	 */
+	private RE re() {
+		RE term = term();
+
+		if (more() && peek() == '|') {
+			eat('|');
+			RE regex = re();
+			return new Choice(term, regex);
+		} else {
+			return term;
+		}
 	}
 
-	/* Recursive descent parsing internals. */
+	/**
+	 * gets the term/factor
+	 * 
+	 * @return - term/factor
+	 */
+	private RE term() {
+		RE term = RE.blank;
 
+		while (more() && peek() != ')' && peek() != '|') {
+			RE nextFactor = factor();
+			term = new Sequence(term, nextFactor);
+		}
+
+		return term;
+	}
+
+	/**
+	 * gets the next factor
+	 * 
+	 * @return
+	 */
+	private RE factor() {
+		RE base = base();
+
+		while (more() && peek() == '*') {
+			eat('*');
+			base = new Repetition(base);
+		}
+
+		return base;
+	}
+
+	/**
+	 * returns a base
+	 * @return - base 
+	 */
+	private RE base() {
+		switch (peek()) {
+		case '(':
+			eat('(');
+			RE r = re();
+			eat(')');
+			return r;
+
+		case '\\':
+			eat('\\');
+			char esc = next();
+			return new Primitive(esc);
+
+		default:
+			return new Primitive(next());
+		}
+	}
+
+	/**
+	 * looks at next char
+	 * @return
+	 */
 	private char peek() {
 		return regEx.charAt(0);
 	}
 
+	/**
+	 * remove next char
+	 * @param c
+	 */
 	private void eat(char c) {
 		if (peek() == c)
 			this.regEx = this.regEx.substring(1);
@@ -41,69 +112,99 @@ public class RE implements REInterface {
 			throw new RuntimeException("Expected: " + c + "; got: " + peek());
 	}
 
+	/**
+	 * gets and removes next char
+	 * @return
+	 */
 	private char next() {
 		char c = peek();
 		eat(c);
 		return c;
 	}
 
-	/* Regular expression term types. */
-
-//	private RE term() {
-//		RE factor = RE.blank ;
-//
-//	    while (more() && peek() != ')' && peek() != '|') {
-//	      RE nextFactor = factor() ;
-//	      factor = new Sequence(factor,nextFactor) ;
-//	    }
-//
-//	    return factor ;
-//	}
-
-	private RE factor() {
-		return null;
-//		RegEx base = base() ;
-//	    
-//	    while (more() && peek() == '*') {
-//	      eat('*') ;
-//	      base = new Repetition(base) ;
-//	    }
-//
-//	    return base ;
-	}
-
-	private RE base() {
-		return null;
-//		switch (peek()) {
-//	      case '(':
-//	        eat('(') ;
-//	        RegEx r = regex() ;  
-//	        eat(')') ;
-//	      return r ;
-//
-//	      case '\\':
-//	       eat ('\\') ;
-//	       char esc = next() ;
-//	      return new Primitive(esc) ;
-//
-//	      default:
-//	      return new Primitive(next()) ;
-//	    }
-	}
-
+	/**
+	 * still more stuff check
+	 * @return
+	 */
 	private boolean more() {
 		return regEx.length() > 0;
 	}
-//Some issue with this	
-//	private class Choice extends RE
-//	{
-//	  private RE thisOne ;
-//	  private RE thatOne ;
-//
-//	  public Choice(RE thisOne, RE thatOne) {
-//	    this.thisOne = thisOne ;
-//	    this.thatOne = thatOne ;
-//	  }
-//	}
 
+
+	/**
+	 * private inner class.
+	 * there is a choice indicated by |
+	 * 
+	 * @author annebrinegar, Megan Pierce
+	 *
+	 */
+	private class Choice extends RE {
+		private RE thisOne;
+		private RE thatOne;
+
+		public Choice(RE thisOne, RE thatOne) {
+			super(regEx);
+			this.thisOne = thisOne;
+			this.thatOne = thatOne;
+		}
+	}
+
+	/**
+	 * private inner class.
+	 * Sequence of characters
+	 * 
+	 * @author annebrinegar, Megan Pierce
+	 *
+	 */
+	private class Sequence extends RE {
+		private RE first;
+		private RE second;
+
+		public Sequence(RE first, RE second) {
+			super(regEx);
+			this.first = first;
+			this.second = second;
+		}
+	}
+
+	/**
+	 * empty character representation
+	 * 
+	 * @author annebrinegar, Megan Pierce
+	 *
+	 */
+	private class Blank extends RE {
+
+		public Blank(String regEx) {
+			super(regEx);
+		}
+	}
+
+	/**
+	 * indicates star?
+	 * 
+	 * @author annebrinegar, Megan Pierce
+	 *
+	 */
+	private class Repetition extends RE {
+		private RE internal;
+
+		public Repetition(RE internal) {
+			super(regEx);
+			this.internal = internal;
+		}
+	}
+
+	/**
+	 * bare bones, individual char.
+	 * @author annebrinegar, Megan Pierce
+	 *
+	 */
+	private class Primitive extends RE {
+		private char c;
+		public Primitive(char c) {
+			super(regEx);
+			this.c = c;
+		}
+	}
 }
